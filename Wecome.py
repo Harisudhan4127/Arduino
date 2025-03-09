3c7b85f3bb499609dfa5567c85977705b40a3b18
Harisudhan Baskaran <harisudhan4127@gmail.com>
2025-03-09 21:14:10 +0530

    Python file

---
+++ Wecome.py
@@ -0,0 +1,8 @@
+import os
+import time as t
+print("Welcome")
+t.sleep(2)
+# print("Current Time : ", t.time())
+# print("Local time : ", t.localtime())
+print(t.strftime("Date : %d-%m-%Y time : %h:%M:%S", t.localtime()))
+# os.system("cls")
